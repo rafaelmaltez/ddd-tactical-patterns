0084591cd7b81103faccc6e8d06cb6256bf424d5
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-01-18 08:39:50 -0300

    add product

---
+++ src/entity/product.ts
@@ -0,0 +1,30 @@
+export default class Product {
+  private _id: string;
+  private _name: string;
+  private _price: number;
+
+  constructor(id: string, name: string, price: number) {
+    this._id = id;
+    this._name = name;
+    this._price = price;
+    this.validate();
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error("Id cannot be empty");
+    }
+  }
+}
