47779d1f9b04235105c5e860d576b46ba9601c25
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-01-17 21:02:49 -0300

    add Address value object

---
+++ src/entity/address.ts
@@ -0,0 +1,35 @@
+export default class Address {
+
+  private _street: string;
+  private _number: number;
+  private _city: string;
+  private _zip: string;
+  private _state: string;
+
+  constructor(street: string, number: number, zip: string, city: string, state: string) {
+    this._street = street;
+    this._number = number;
+    this._city = city;
+    this._zip = zip;
+    this._state = state;
+    this.validate();
+  }
+
+  validate() {
+    if (this._street.length < 2) {
+      throw new Error("Street must be at least 2 characters long");
+    }
+    if (this._number < 0) {
+      throw new Error("Number must be positive");
+    }
+    if (this._city.length < 2) {
+      throw new Error("City must be at least 2 characters long");
+    }
+    if (this._zip.length < 2) {
+      throw new Error("Zip must be at least 2 characters long");
+    }
+    if (this._state.length < 2) {
+      throw new Error("State must be at least 2 characters long");
+    }
+  }
+}
