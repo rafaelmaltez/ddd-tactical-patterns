11e795771d8d31f78f15604fc2f14b2ae06d5240
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-01-18 08:08:22 -0300

    test Customer should throw when name is less than 2 characters

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -3,5 +3,8 @@ import Customer from './customer';
 describe('Customer unit tests', () => {
   it('Should throw when id is empty', () => {
     expect(() => new Customer('', 'John Doe')).toThrow("Id cannot be empty");
-  })
+  });
+  it('Should throw when name is less than 2 characters', () => {
+    expect(() => new Customer('1', 'J')).toThrow("Name must be at least 2 characters long");
+  });
 })
