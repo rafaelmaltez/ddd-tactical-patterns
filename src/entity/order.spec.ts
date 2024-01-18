c379f0994e410dc04f9c2cf0826cda34332d1f2d
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-01-18 08:32:20 -0300

    test Order should calculate total

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -1,4 +1,5 @@
 import Order from './order';
+import OrderItem from './order_item';
 
 describe('Order unit tests', () => {
   it('Should throw when id is empty', () => {
@@ -10,4 +11,10 @@ describe('Order unit tests', () => {
   it('Should throw when items is empty', () => {
     expect(() => new Order('1', '1', [])).toThrow("Items cannot be empty");
   });
+  it('Should calculate total', () => {
+    const item1 = new OrderItem('1', 'item1', 10);
+    const item2 = new OrderItem('2', 'item2', 20);
+    const order = new Order('1', '1', [item1, item2]);
+    expect(order.total()).toBe(30);
+  })
 })
