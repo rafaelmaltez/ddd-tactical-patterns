1b975c891cd23a0cbe0d92ca3b7427e9ec35f562
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-01-25 08:11:10 -0300

    add Product Service

---
+++ src/service/product.service.spec.ts
@@ -0,0 +1,14 @@
+import Product from '../entity/product';
+import ProductService from './product.service';
+
+describe('Product service unit tests', () => {
+  it('Should change the price of all products', () => {
+    const product1 = new Product('1', 'Product 1', 150);
+    const product2 = new Product('2', 'Product 2', 250);
+
+    ProductService.changePrice([product1, product2], 100);
+
+    expect(product1.price).toBe(300);
+    expect(product2.price).toBe(500);
+  })
+})
