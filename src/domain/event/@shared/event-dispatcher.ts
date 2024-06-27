3e5caac13224e681551dfaa83b425c0261287fcb
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-06-27 20:45:42 -0300

    add event dispatcher

---
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -0,0 +1,31 @@
+import EventDispatchInterface from "./event-dispatcher.interface";
+import EventHandlerInterface from "./event-handler.interface";
+import EventInterface from "./event.interface";
+
+export default class EventDispatcher implements EventDispatchInterface{
+    private eventHandlers: {[eventName: string]: Set<EventHandlerInterface<EventInterface>>} = {};
+
+    get getEventHandlers(): {[eventName: string]: Set<EventHandlerInterface<EventInterface>>} {
+        return this.eventHandlers;
+    }
+
+    register(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
+        if (!this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName] = new Set();
+        }
+        this.eventHandlers[eventName].add(eventHandler);
+    }
+
+    notify(event: EventInterface): void {
+        throw new Error("Method not implemented.");
+    }
+
+    unregister(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
+        throw new Error("Method not implemented.");
+    }
+
+    unregisterAll(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
