345ac70c9a9561eefeab524424543dd62d32f2c2
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-06-27 20:55:32 -0300

    removes unnecessary generics from test

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -3,13 +3,13 @@ import EventHandlerInterface from "./event-handler.interface";
 import EventInterface from "./event.interface";
 
 export default class EventDispatcher implements EventDispatchInterface{
-    private eventHandlers: {[eventName: string]: Set<EventHandlerInterface<EventInterface>>} = {};
+    private eventHandlers: {[eventName: string]: Set<EventHandlerInterface>} = {};
 
-    get getEventHandlers(): {[eventName: string]: Set<EventHandlerInterface<EventInterface>>} {
+    get getEventHandlers(): {[eventName: string]: Set<EventHandlerInterface>} {
         return this.eventHandlers;
     }
 
-    register(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
+    register(eventName: string, eventHandler: EventHandlerInterface): void {
         if (!this.eventHandlers[eventName]) {
             this.eventHandlers[eventName] = new Set();
         }
@@ -20,7 +20,7 @@ export default class EventDispatcher implements EventDispatchInterface{
         throw new Error("Method not implemented.");
     }
 
-    unregister(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
+    unregister(eventName: string, eventHandler: EventHandlerInterface): void {
         if (!this.eventHandlers[eventName]) {
             return;
         }
